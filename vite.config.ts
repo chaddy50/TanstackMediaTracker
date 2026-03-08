b093d3bee1efc8bf8be058c3503de276ef25e857
chaddy50 <nchaddy50@protonmail.com>
2026-03-08 12:51:30 -0500

    Configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const config = defineConfig({
   plugins: [
@@ -16,6 +17,29 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: null,
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,ico,png,svg,woff2}'],
+        navigateFallback: null,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.+\.(png|jpg|jpeg|webp|svg)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'cover-images',
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+              cacheableResponse: { statuses: [0, 200] },
+            },
+          },
+        ],
+      },
+    }),
   ],
 })
 
